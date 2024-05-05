458d3f5cbddf73c5d345f868c3c8342a1f696dff
Daniil-Solo <daniil.solo1723@gmail.com>
2024-05-05 15:13:28 +0300

    add complex model instead of simple model

--- dvc_scripts/fit.py
+++ dvc_scripts/fit.py
@@ -6,7 +6,7 @@ import click
 import dvc.api
 import pandas as pd
 from scipy import sparse
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 
 @click.command()  # type: ignore
@@ -24,14 +24,14 @@ def cli(input_dataset_file: str, features_file: str, output_model_file: str) -> 
     # loading parameters
     params = dvc.api.params_show()
     random_state = params["random_state"]
-    model_params = params["logistic_regression"]
+    model_params = params["random_forest"]
     # loading data
     df = pd.read_csv(input_dataset_file)
     target = df["target"]
     del df
     data = sparse.load_npz(features_file)
     # model training
-    clf = LogisticRegression(random_state=random_state, **model_params)
+    clf = RandomForestClassifier(random_state=random_state, **model_params)
     clf.fit(data, target)
     # model saving
     with open(output_model_file, "wb") as f:
